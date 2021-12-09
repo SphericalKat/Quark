4e1febeea71dc505ff4bef85b61bd87a38b0ea7b
SphericalKat <amolele@gmail.com>
2021-12-10 00:12:21 +0530

    chore: update to tailwind 3.0
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- src/pages/Subscriptions.tsx
+++ src/pages/Subscriptions.tsx
@@ -5,9 +5,9 @@ const Subscriptions: Component = () => {
   return (
     <div class="flex flex-col items-center justify-center w-full h-full">
       <div class="h-full w-full">Subs content placeholder</div>
-      <div class="flex w-full px-4 py-2">
-        <Link href="/">Home</Link>
-        <Link href="/subs">Subscriptions</Link>
+      <div class="flex w-full px-4 py-4 border border-t">
+        <Link href="/" class="flex-1 text-center">Home</Link>
+        <Link href="/subs" class="flex-1 text-center">Subscriptions</Link>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,12 @@ const colors = require("tailwindcss/colors");
 
 module.exports = {
   mode: "jit",
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        gray: colors.trueGray,
+        gray: colors.neutral,
       },
     },
   },
