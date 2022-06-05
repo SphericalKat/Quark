aad8c347a915bcb431c6a04dab06ef31fe31324d
Amogh Lele <amolele@gmail.com>
2022-06-06 02:05:44 +0530

    feat: add PWA stuff
    Signed-off-by: Amogh Lele <amolele@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,46 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(),
+    VitePWA({
+      includeAssets: [
+        "favicon.svg",
+        "favicon.ico",
+        "robots.txt",
+        "apple-touch-icon.png",
+      ],
+      manifest: {
+        name: "Quark",
+        description: "Minimal RSS feed reader",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
-      'stream': 'stream-browserify',
-    }
+      stream: "stream-browserify",
+    },
   },
   build: {
     target: "esnext",
