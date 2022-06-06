693b04895626f78bf979412d9239e52ece52d354
Amogh Lele <amolele@gmail.com>
2022-06-06 15:58:53 +0530

    feat: add suspense fallback to display seamless background
    Signed-off-by: Amogh Lele <amolele@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "solid-app-router";
-import { Component, lazy } from "solid-js";
+import { Component, lazy, Suspense } from "solid-js";
 
 const Home = lazy(() => import("./pages/Home"));
 const Subscriptions = lazy(() => import("./pages/Subscriptions"));
@@ -7,10 +7,12 @@ const Subscriptions = lazy(() => import("./pages/Subscriptions"));
 const App: Component = () => {
   return (
     <div class="flex flex-col w-full h-full items-center justify-center">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/subs" element={<Subscriptions />} />
-      </Routes>
+      <Suspense fallback={<div class="page-container"></div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/subs" element={<Subscriptions />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
